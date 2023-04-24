fbca9dde3770af58179eb9e6aca1ebd85b5fbf94
Olzhas Arystanov <o.arystanov@gmail.com>
2023-04-24 16:48:25 +0600

    Time: 37 ms (72.03%) | Memory: 13.9 MB (71.78%) - LeetSync

--- merge-sorted-array/merge-sorted-array.py
+++ merge-sorted-array/merge-sorted-array.py
@@ -4,7 +4,15 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
 
-        for i in range(n):
-            nums1[m + i] = nums2[i]
+        nums1_copy = nums1[:m]
 
-        nums1.sort()
+        i = 0
+        j = 0
+
+        for p in range(m + n):
+            if j >= n or (i < m and nums1_copy[i] <= nums2[j]):
+                nums1[p] = nums1_copy[i]
+                i += 1
+            else:
+                nums1[p] = nums2[j]
+                j += 1
