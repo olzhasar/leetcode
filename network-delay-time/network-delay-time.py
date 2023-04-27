42d14f6b9aa2c06b4450d966232fc95b2e197148
Olzhas Arystanov <o.arystanov@gmail.com>
2023-04-28 05:27:29 +0600

    Time: 500 ms (21.71%) | Memory: 18.5 MB (9.38%) - LeetSync

--- network-delay-time/network-delay-time.py
+++ network-delay-time/network-delay-time.py
@@ -4,7 +4,7 @@ import heapq
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         adj_list = defaultdict(list)
-        result = [-1] * (n + 1)
+        result = [float("inf") for _ in range(n + 1)]
 
         for source, target, time in times:
             adj_list[source].append((time, target))
@@ -14,18 +14,16 @@ class Solution:
         while min_heap:
             curr_time, node = heapq.heappop(min_heap)
 
-            if result[node] != -1:
+            if curr_time > result[node]:
                 continue
             result[node] = curr_time
 
             for time, target in adj_list.get(node, []):
-                heapq.heappush(min_heap, (time + curr_time, target))
+                t = time + curr_time 
+                if t < result[target]:
+                    heapq.heappush(min_heap, (time + curr_time, target))
 
-        max_time = 0
-
-        for time in result[1:]:
-            if time == -1:
-                return -1
-            max_time = max(max_time, time)
-
+        max_time = max(result[1:])
+        if max_time == float("inf"):
+            return -1
         return max_time
