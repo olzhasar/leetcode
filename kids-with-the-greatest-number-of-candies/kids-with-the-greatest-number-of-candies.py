7d6ce6edf7297ded4f6964cd1c81ef8a0a5d4c63
Olzhas Arystanov <o.arystanov@gmail.com>
2023-04-28 17:59:45 +0600

    Time: 52 ms (5.45%) | Memory: 16.4 MB (8.58%) - LeetSync

---
+++ kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -0,0 +1,9 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        max_candies = max(candies)
+        result = []
+
+        for i, n in enumerate(candies):
+            result.append(n + extraCandies >= max_candies)
+
+        return result
