33a5ed318522e4993117fb87a01b9b211a3f7241
Olzhas Arystanov <o.arystanov@gmail.com>
2023-04-28 17:29:59 +0600

    Time: 48 ms (7.84%) | Memory: 16.2 MB (9.43%) - LeetSync

--- merge-strings-alternately/merge-strings-alternately.py
+++ merge-strings-alternately/merge-strings-alternately.py
@@ -3,20 +3,14 @@ class Solution:
         len_1, len_2 = len(word1), len(word2)
 
         l, r = 0, 0
-        output = ""
+        output = []
 
         while l < len_1 or r < len_2:
             if l < len_1:
-                output += word1[l]
+                output.append(word1[l])
                 l += 1
-            else:
-                output += word2[r:]
-                break
             if r < len_2:
-                output += word2[r]
+                output.append(word2[r])
                 r += 1
-            else:
-                output += word1[l:]
-                break
 
-        return output
+        return "".join(output)
