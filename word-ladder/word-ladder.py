d5e2212d62b7cdac9c6ca8d2aa7f02d12635a58c
Olzhas Arystanov <o.arystanov@gmail.com>
2023-05-02 01:08:46 +0600

    Time: 8964 ms (5.02%) | Memory: 18 MB (21.53%) - LeetSync

--- word-ladder/word-ladder.py
+++ word-ladder/word-ladder.py
@@ -6,31 +6,32 @@ class Solution:
         paths = defaultdict(list)
 
         wordList.append(beginWord)
-        n = len(wordList)
 
-        for i in range(n):
-            for j in range(i + 1, n):
+        for i, source in enumerate(wordList):
+            for j, word in enumerate(wordList[i:]):
                 diff_count = 0
-                for char1, char2 in zip(wordList[i], wordList[j]):
+                for char1, char2 in zip(source, word):
                     if char1 != char2:
                         diff_count += 1
                         if diff_count > 1:
                             break
                 if diff_count == 1:
-                    paths[i].append(j)
-                    if i != n - 1:
-                        paths[j].append(i)
+                    paths[source].append(word)
+                    if source != beginWord:
+                        paths[word].append(source)
 
-        queue = deque([(n - 1, 1)])
+        queue = deque([(beginWord, 1)])
         visited = set()
 
         while queue:
-            idx, acc_count = queue.popleft()
-            visited.add(idx)
-            if wordList[idx] == endWord:
+            word, acc_count = queue.popleft()
+            if word in visited:
+                continue
+            visited.add(word)
+            if word == endWord:
                 return acc_count
             
-            for path in paths[idx]:
+            for path in paths[word]:
                 if path not in visited:
                     queue.append((path, acc_count + 1))
 
