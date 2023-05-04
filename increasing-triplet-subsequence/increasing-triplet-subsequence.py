0c9ab20c71f7f504d68945106bc22e89b09065bc
Olzhas Arystanov <o.arystanov@gmail.com>
2023-05-05 03:44:33 +0600

    Time: 1457 ms (8.63%) | Memory: 32.4 MB (11.53%) - LeetSync

--- increasing-triplet-subsequence/increasing-triplet-subsequence.py
+++ increasing-triplet-subsequence/increasing-triplet-subsequence.py
@@ -8,8 +8,8 @@ class Solution:
         for i in range(n):
             min_so_far = min(min_so_far, nums[i])
             mins[i] = min_so_far
-            max_so_far = max(max_so_far, nums[n - 1 - i])
-            maxs[n - 1 - i] = max_so_far
+            max_so_far = max(max_so_far, nums[-1 - i])
+            maxs[-1 - i] = max_so_far
 
         for i in range(1, n - 1):
             if mins[i - 1] < nums[i] < maxs[i + 1]:
