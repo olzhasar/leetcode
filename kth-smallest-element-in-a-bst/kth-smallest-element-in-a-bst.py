0cbae98bc6a2f206ba397a1af5e5ded7e2a17602
Olzhas Arystanov <o.arystanov@gmail.com>
2023-05-09 01:05:12 +0600

    Time: 65 ms (15.42%) | Memory: 20.5 MB (5.16%) - LeetSync

--- kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -1,4 +1,3 @@
-import heapq
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,21 +6,19 @@ import heapq
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        heap = []
+        vals = []
         
         def dfs(node):
             if not node:
                 return
             
-            heapq.heappush(heap, node.val)
+            vals.append(node.val)
             
             if node.left:
                 dfs(node.left)
             if node.right:
                 dfs(node.right)
-                
+
         dfs(root)
-        for _ in range(k - 1):
-            heapq.heappop(heap)
-            
-        return heapq.heappop(heap)
+                
+        return sorted(vals)[k - 1]
