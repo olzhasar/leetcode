53b81c6d0d4e461a517f9a038461a816049c8ac8
Olzhas Arystanov <o.arystanov@gmail.com>
2023-05-12 03:05:15 +0600

    Time: 88 ms (64.89%) | Memory: 23.1 MB (16.21%) - LeetSync

--- counting-bits/counting-bits.py
+++ counting-bits/counting-bits.py
@@ -5,8 +5,6 @@ class Solution:
         def num_bits(n):
             if n == 0:
                 return 0
-            if n == 1:
-                return 1
             return result[n // 2] + n % 2
 
         for i in range(n + 1):
