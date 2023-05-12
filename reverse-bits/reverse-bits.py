6a1e9c32942dbf531c6a0e8c24172e0c123461eb
Olzhas Arystanov <o.arystanov@gmail.com>
2023-05-12 15:11:09 +0600

    Time: 52 ms (8.04%) | Memory: 16.3 MB (9.60%) - LeetSync

--- reverse-bits/reverse-bits.py
+++ reverse-bits/reverse-bits.py
@@ -1,13 +1,10 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        bits = [0] * 32
-
         result = 0
 
         i = 0
         while i < 32 and n > 0:
             if n % 2 == 1:
-                bits[i] = 1
                 result += 2 ** (31 - i)
             n //= 2
             i += 1
