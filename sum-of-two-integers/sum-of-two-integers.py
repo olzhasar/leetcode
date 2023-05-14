3e2760e9a4367bfa15b7900ef0b6193851f62eae
Olzhas Arystanov <o.arystanov@gmail.com>
2023-05-14 11:29:15 +0600

    Time: 50 ms (7.52%) | Memory: 16.3 MB (14.80%) - LeetSync

--- sum-of-two-integers/sum-of-two-integers.py
+++ sum-of-two-integers/sum-of-two-integers.py
@@ -2,21 +2,14 @@ class Solution:
     def getSum(self, a: int, b: int) -> int:
         mask = 0xFFFFFFFF
         sign_bit = 0x80000000
-        max_val = 0x7FFFFFFF
-    
+
         while b != 0:
-            # Calculate the sum without considering the carry
-            sum = (a ^ b) & mask
-    
-            # Calculate the carry, considering sign extension
+            s = (a ^ b) & mask
             carry = ((a & b) << 1) & mask
-    
-            # Update a and b for the next iteration
-            a = sum
-            b = carry
-    
-        # Handle negative overflow
+
+            a, b = s, carry
+
         if a & sign_bit:
-            a = -((~a + 1) & max_val)
-    
+            a = a | ~mask
+
         return a
