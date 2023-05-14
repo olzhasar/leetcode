0397e3b915c67176ec9928b9de77bc48b44d8bbe
Olzhas Arystanov <o.arystanov@gmail.com>
2023-05-15 00:45:47 +0600

    Time: 101 ms (43.47%) | Memory: 23.2 MB (8.10%) - LeetSync

--- counting-bits/counting-bits.py
+++ counting-bits/counting-bits.py
@@ -3,6 +3,6 @@ class Solution:
         result = [0] * (n + 1)
 
         for i in range(1, n + 1):
-            result[i] = result[i // 2] + i % 2
+            result[i] = result[i >> 1] + i % 2
 
         return result
