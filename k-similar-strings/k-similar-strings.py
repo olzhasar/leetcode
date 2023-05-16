9ec38f37e6bf0990e5066f7f9ee09e98493937f9
Olzhas Arystanov <o.arystanov@gmail.com>
2023-05-17 01:46:34 +0600

    Time: 509 ms (33.33%) | Memory: 23 MB (23.12%) - LeetSync

---
+++ k-similar-strings/k-similar-strings.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+
+class Solution:
+    def kSimilarity(self, s1: str, s2: str) -> int:
+        queue = deque([s1])
+        length = len(s1)
+        visited = {s1}
+        k = 0
+
+        while queue:
+            for _ in range(len(queue)):
+                current = queue.popleft()
+
+                if current == s2:
+                    return k
+
+                i = 0
+
+                while current[i] == s2[i]:
+                    i += 1
+
+                for j in range(i, length):
+                    if current[j] == s2[i]:
+                        new_state = current[:i] + current[j] + current[i + 1:j] + current[i] + current[j + 1:]
+                        if new_state not in visited:
+                            queue.append(new_state)
+                            visited.add(new_state)
+
+            k += 1
+
+        return -1
