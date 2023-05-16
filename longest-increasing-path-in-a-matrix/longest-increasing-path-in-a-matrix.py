14331f331017e69a21483c656957d1b4e5f1f46b
Olzhas Arystanov <o.arystanov@gmail.com>
2023-05-17 00:07:36 +0600

    Time: 429 ms (72.21%) | Memory: 17.2 MB (59.56%) - LeetSync

--- longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
+++ longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
@@ -1,14 +1,15 @@
-import functools
-
-
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         longest = 0
         m = len(matrix)
         n = len(matrix[0])
 
-        @functools.cache
+        dp = [[-1] * n for _ in range(m)]
+
         def dfs(r, c):
+            if dp[r][c] != -1:
+                return dp[r][c]
+
             directions = (
                 (1, 0),
                 (-1, 0),
@@ -22,7 +23,8 @@ class Solution:
                 if 0 <= dr + r < m and 0 <= dc + c < n and matrix[dr + r][dc + c] > matrix[r][c]:
                     max_result = max(max_result, dfs(dr + r, dc + c))
 
-            return max_result + 1
+            dp[r][c] = max_result + 1
+            return dp[r][c]
 
         for r in range(m):
             for c in range(n):
