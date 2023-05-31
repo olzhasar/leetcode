b570209326f5510f3f36849e81ccf9807a92b0fa
Olzhas Arystanov <o.arystanov@gmail.com>
2023-05-31 20:42:54 +0600

    Time: 232 ms (60.56%) | Memory: 16.4 MB (32.14%) - LeetSync

--- sudoku-solver/sudoku-solver.py
+++ sudoku-solver/sudoku-solver.py
@@ -3,38 +3,38 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        rows = [set() for _ in range(9)]
-        cols = [set() for _ in range(9)]
-        boxes = [set() for _ in range(9)]
+        rows = [[0] * 9 for _ in range(9)]
+        cols = [[0] * 9 for _ in range(9)]
+        boxes = [[0] * 9 for _ in range(9)]
 
         _get_box_num = lambda r, c:  r // 3 * 3 + c // 3
 
         for r in range(9):
             for c in range(9):
                 if board[r][c] != ".":
-                    rows[r].add(board[r][c])
-                    cols[c].add(board[r][c])
-                    boxes[_get_box_num(r, c)].add(board[r][c])
+                    val = int(board[r][c]) - 1
+                    rows[r][val] = 1
+                    cols[c][val] = 1
+                    boxes[_get_box_num(r, c)][val] = 1
 
         def backtrack() -> bool:
             for r in range(9):
                 for c in range(9):
                     if board[r][c] == ".":
                         box_num = _get_box_num(r, c)
-                        for k in range(1, 10):
-                            val = str(k)
-                            if val in rows[r] or val in cols[c] or val in boxes[box_num]:
+                        for k in range(9):
+                            if rows[r][k] or cols[c][k] or boxes[box_num][k]:
                                 continue
-                            board[r][c] = val
-                            rows[r].add(val)
-                            cols[c].add(val)
-                            boxes[box_num].add(val)
+                            board[r][c] = str(k + 1)
+                            rows[r][k] = 1
+                            cols[c][k] = 1
+                            boxes[box_num][k] = 1
                             if backtrack():
                                 return True
                             board[r][c] = "."
-                            rows[r].discard(val)
-                            cols[c].discard(val)
-                            boxes[box_num].discard(val)
+                            rows[r][k] = 0
+                            cols[c][k] = 0
+                            boxes[box_num][k] = 0
                         return False
 
             return True
